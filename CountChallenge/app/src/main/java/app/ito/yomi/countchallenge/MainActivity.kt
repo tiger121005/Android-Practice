c7938dbeb7eb3341952517d9d66641d0447f7454
tiger121005 <tiger121005@github.com\nalkvjn>
2024-08-26 15:40:17 +0900

    CountChallenge 5

--- CountChallenge/app/src/main/java/app/ito/yomi/countchallenge/MainActivity.kt
+++ CountChallenge/app/src/main/java/app/ito/yomi/countchallenge/MainActivity.kt
@@ -2,8 +2,10 @@ package app.ito.yomi.countchallenge
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,24 +18,31 @@ class MainActivity : AppCompatActivity() {
 
     var count = 0
 
-    var prefs: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
         val sound: MediaPlayer = MediaPlayer.create(this, R.raw.tukkomi)
+        prefs = getPreferences(Context.MODE_PRIVATE)
 
+        count = prefs.getInt("COUNT", 0)
+        Log.d("debug", count.toString())
         binding.textView.text = count.toString()
 
         binding.button.setOnClickListener {
             count++
             binding.textView.text = count.toString()
 
+            val editor: SharedPreferences.Editor = prefs.edit()
+            editor.putInt("COUNT", count)
+            editor.apply()
+
             when (count%2) {
-                0 -> binding.textView.setTextColor(resources.getColor(R.color.blue))
+                0 -> binding.textView.setTextColor(Color.BLUE)
                 else -> {
-                    binding.textView.setTextColor(resources.getColor(R.color.red))
+                    binding.textView.setTextColor(Color.RED)
                     sound.seekTo(0)
                     sound.start()
                 }
@@ -41,6 +50,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {
+        super.onPause()
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
 
 }
